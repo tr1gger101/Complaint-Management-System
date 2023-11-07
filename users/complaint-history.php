99847d4411702d41a90bb16e584d4c1e47f277f2
Harshit Dharani <34885012+tr1gger101@users.noreply.github.com>
2023-11-07 12:22:12 +0530

    Add files via upload

---
+++ users/complaint-history.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+  {
+header('location:index.php');
+}
+if(isset($_GET['del']))
+		  {
+		          mysqli_query($bd, "delete from tblcomplaints where complaintNumber = '".$_GET['cid']."'");
+                  header("Refresh:0; url=complaint-history.php");
+		  }
+else{
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>CMS | Complaint History</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
+
+    <link href="assets/css/table-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+  <section id="container" >
+<?php include("includes/header.php");?>
+<?php include("includes/sidebar.php");?>
+
+      <section id="main-content">
+          <section class="wrapper">
+          	<h3><i class="fa fa-angle-right"></i>Your Complaint History</h3>
+		  		<div class="row mt">
+			  		<div class="col-lg-12">
+                      <div class="content-panel">
+                          <section id="unseen">
+                            <table class="table table-bordered table-striped table-condensed">
+                              <thead>
+                              <tr>
+                                  <th>Complaint Number</th>
+                                  <th>Reg Date</th>
+                                  <th>Last Updation date</th>
+                                  <th >Status</th>
+                                  <th>Action</th>
+                                  <th>Withdraw</th>
+                              </tr>
+                              </thead>
+                              <tbody>
+  <?php $query=mysqli_query($bd, "select * from tblcomplaints where userId='".$_SESSION['id']."'");
+while($row=mysqli_fetch_array($query))
+{
+  ?>
+                              <tr>
+                                  <td align="center"><?php echo htmlentities($row['complaintNumber']);?></td>
+                                  <td align="center"><?php echo htmlentities($row['regDate']);?></td>
+                                 <td align="center"><?php echo  htmlentities($row['lastUpdationDate']);
+
+                                 ?></td>
+                                  <td align="center"><?php
+                                    $status=$row['status'];
+                                    if($status=="" or $status=="NULL")
+                                    { ?>
+                                      <button type="button" class="btn btn-theme04">Not Processed Yet</button>
+                                   <?php }
+ if($status=="in process"){ ?>
+<button type="button" class="btn btn-warning">In Process</button>
+<?php }
+if($status=="closed") {
+?>
+<button type="button" class="btn btn-success">Closed</button>
+<?php } ?>
+                                   <td align="center">
+                                   <a href="complaint-details.php?cid=<?php echo htmlentities($row['complaintNumber']);?>">
+<button type="button" class="btn btn-primary">View Details</button></a>
+                                   </td>
+                                   <td align="center">
+                                   <a href="complaint-history.php?cid=<?php echo htmlentities($row['complaintNumber']);?>&del=delete">
+<button type="button" class="btn btn-theme04" onClick="return confirm('Are you sure you want to delete?')">Delete Complaint</button></a>
+                                   </td>
+                                </tr>
+                              <?php } ?>
+
+                              </tbody>
+                          </table>
+                          </section>
+                  </div><!-- /content-panel -->
+               </div><!-- /col-lg-4 -->
+		  	</div><!-- /row -->
+
+
+
+		</section><! --/wrapper -->
+      </section><!-- /MAIN CONTENT -->
+<?php include("includes/footer.php");?>
+  </section>
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="assets/js/jquery.scrollTo.min.js"></script>
+    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+
+
+    <!--common script for all pages-->
+    <script src="assets/js/common-scripts.js"></script>
+
+    <!--script for this page-->
+
+
+  </body>
+</html>
+<?php } ?>
